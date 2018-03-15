93af4ee403dc86c527bf70cf2438ac2a9952f447
Yoichiro Tanaka <yoichiro@eisbahn.jp>
2018-03-15 23:12:05 +0900

    Localizing into Japanese.

--- index.js
+++ index.js
@@ -11,95 +11,141 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-'use strict';
+"use strict";
 
-process.env.DEBUG = 'actions-on-google:*';
+process.env.DEBUG = "actions-on-google:*";
 
-// We need the Assistant client for all the magic here
-const Assistant = require('actions-on-google').ApiAiAssistant;
-// To make our http request (a bit nicer)
-const request = require('request');
+// Dialogflowを利用するためのモジュールを読み込みます。
+const DialogflowApp = require("actions-on-google").DialogflowApp;
+// 外部APIを利用するためのモジュールを読み込みます。
+const request = require("request");
 
-// the actions we are supporting (get them from api.ai)
-const ACTION_PRICE = 'price';
-const ACTION_TOTAL = 'total';
-const ACTION_BLOCK = 'block';
-const ACTION_MARKET = 'marketcap';
-const ACTION_INTERVAL = 'interval';
-
-// The end-points to our calls
+// DialogflowからWebhookで呼び出される際のactionの定数です。
+const ACTION_PRICE = "price";
+const ACTION_TOTAL = "total";
+// BitcoinのAPIのURLおよびPathの定数です。
 const EXT_BITCOIN_API_URL = "https://blockchain.info";
 const EXT_PRICE = "/q/24hrprice";
 const EXT_TOTAL = "/q/totalbc";
-const EXT_BLOCK_COUNT = "/q/getblockcount";
-const EXT_MARKET_CAP = "/q/marketcap";
-const EXT_INTERVAL = "/q/interval";
 
-// [START Bitcoin Info]
-const bitcoinInfo = (req, res) => {
-  const assistant = new Assistant({request: req, response: res});
-  console.log('bitcoinInfoAction Request headers: ' + JSON.stringify(req.headers));
-  console.log('bitcoinInfoAction Request body: ' + JSON.stringify(req.body));
+// [START bitcoinInfo]
+// 最初の "bitcoinInfo" Cloud Function の定義です。
+exports.bitcoinInfo = (req, res) => {
+    // DialogflowAppのインスタンスを生成します。
+    const app = new DialogflowApp({request: req, response: res});
 
-  // Fulfill price action business logic
-  function priceHandler (assistant) {
-    request(EXT_BITCOIN_API_URL + EXT_PRICE, function(error, response, body) {
-      // The fulfillment logic for returning the bitcoin current price
-      console.log("priceHandler response: " + JSON.stringify(response) + " Body: " + body + " | Error: " + error);
-      const msg = "Right now the price of a bitcoin is " + body + " USD. What else would you like to know?";
-      assistant.ask(msg);
-    });
-  }
+    // DialogflowからWebhookで呼び出された際のリクエストの内容をログ出力します。
+    console.log(`bitcoinInfoAction Request headers: ${JSON.stringify(req.headers)}`);
+    console.log(`bitcoinInfoAction Request body: ${JSON.stringify(req.body)}`);
 
-  // Fulfill total bitcoin action 
-  function totalHandler (assistant) {
-    request(EXT_BITCOIN_API_URL + EXT_TOTAL, function(error, response, body) {
-      console.log("totalHandler response: " + JSON.stringify(response) + " Body: " + body + " | Error: " + error);
-      // The fulfillment logic for returning the amount of bitcoins in the world
-      const billionsBitcoins = body / 1000000000;
-      const msg = "Right now there are " + billionsBitcoins + " billion bitcoins around the world. What else would you like to know?";
-      assistant.ask(msg);
-    });
-  }
-  
-  // Fulfill block count action
-  function blockCountHandler (assistant) {
-    request(EXT_BITCOIN_API_URL + EXT_BLOCK_COUNT, function(error, response, body) {
-      console.log("blockCountHandler response: " + JSON.stringify(response) + " Body: " + body + " | Error: " + error);
-      const msg = "Right now there are " + body + " blocks. What else would you like to know? the price?";
-      assistant.ask(msg);
-    });
-  }
+    // Bitcoinの価格をアメリカドルで返事する "price" アクションのハンドラ関数です。
+    const priceHandler = (app) => {
+        // Bitcoin APIにアクセスして、Bitcoinの価格を得ます。
+        // Response Bodyには価格のみが送られてきます。
+        request(EXT_BITCOIN_API_URL + EXT_PRICE, (error, response, body) => {
+            // Bitcoin APIの結果をログ出力します。
+            console.log(`priceHandler response: ${JSON.stringify(response)} Body: ${body} | Error: ${error}`);
+            // 返事のメッセージ文字列を組み立てます。
+            const msg = `現在のビットコインの価格は、${body}アメリカドルです`;
+            // tell 関数を使って返事を行い、会話を終了します。
+            app.tell(msg);
+        });
+    };
 
-  // Fulfill market cap action
-  function marketCaptHandler (assistant) {
-    request(EXT_BITCOIN_API_URL + EXT_MARKET_CAP, function(error, response, body) {
-      console.log("marketCaptHandler response: " + JSON.stringify(response) + " Body: " + body + " | Error: " + error);
-      const marketCapB = Math.round((body / 1000000000) * 100) / 100
-      const msg = "Right now market cap is " + marketCapB + " billions. What else would you like to know?";
-      assistant.ask(msg);
-    });
-  }
-  
-  // Fulfill interval action
-  function intervalHandler (assistant) {
-    request(EXT_BITCOIN_API_URL + EXT_INTERVAL, function(error, response, body) {
-      console.log("interval response: " + JSON.stringify(response) + " Body: " + body + " | Error: " + error);
-      const msg = "Right now the interval between blocks is " + body + " seconds. What else would you like to know?";
-      assistant.ask(msg);
-    });
-  }
+    // Bitcoinの総数を返事する "total" アクションのハンドラ関数です。
+    const totalHandler = (app) => {
+        // Bitcoin APIにアクセスして、Bitcoinの総数を得ます。
+        // Response Bodyには総数のみが送られてきます。
+        request(EXT_BITCOIN_API_URL + EXT_TOTAL, (error, response, body) => {
+            // Bitcoin APIの結果をログ出力します。
+            console.log(`totalHandler response: ${JSON.stringify(response)} Body: ${body} | Error: ${error}`);
+            // 総数の桁が大きいので、億の単位にします。
+            const total = body / 100000000;
+            // 返事のメッセージ文字列を組み立てます。
+            const msg = `現在、世界中で${total}億のビットコインがあります`;
+            // tell 関数を使って返事を行い、会話を終了します。
+            app.tell(msg);
+        });
+    };
 
-  // The Entry point to all our actions
-  const actionMap = new Map();
-  actionMap.set(ACTION_PRICE, priceHandler);
-  actionMap.set(ACTION_TOTAL, totalHandler);
-  actionMap.set(ACTION_BLOCK, blockCountHandler);
-  actionMap.set(ACTION_MARKET, marketCaptHandler);
-  actionMap.set(ACTION_INTERVAL, intervalHandler);
-  
-  assistant.handleRequest(actionMap);
+    // アクションのハンドラ関数のマップを作ります。
+    const actionMap = new Map();
+    actionMap.set(ACTION_PRICE, priceHandler);
+    actionMap.set(ACTION_TOTAL, totalHandler);
+
+    // ハンドラ関数のマップを渡して、リクエストを処理します。
+    app.handleRequest(actionMap);
 };
-// [END Bitcoin Info]
-const functions = require('firebase-functions');
-exports.bitcoinInfo = functions.https.onRequest( bitcoinInfo );
+// [END bitcoinInfo]
+
+// [START bitcoinInfo2]
+// 通貨の変換処理を追加した "bitcoinInfo2" Cloud Function の定義です。
+exports.bitcoinInfo2 = (req, res) => {
+    // DialogflowAppのインスタンスを生成します。
+    const app = new DialogflowApp({request: req, response: res});
+
+    // DialogflowからWebhookで呼び出された際のリクエストの内容をログ出力します。
+    console.log(`bitcoinInfoAction Request headers: ${JSON.stringify(req.headers)}`);
+    console.log(`bitcoinInfoAction Request body: ${JSON.stringify(req.body)}`);
+
+    // Bitcoinの価格を指定された通貨で返事する "price" アクションのハンドラ関数です。
+    const priceHandler = (app) => {
+        // Bitcoin APIにアクセスして、Bitcoinの価格を得ます。
+        // Response Bodyには価格のみが送られてきます。
+        request(EXT_BITCOIN_API_URL + EXT_PRICE, (error, response, body) => {
+            // Bitcoin APIの結果をログ出力します。
+            console.log(`priceHandler response: ${JSON.stringify(response)} body: ${body} | error: ${error}`);
+            // 総数を数値に変換します。
+            let price = Number(body);
+            // 通貨レートAPIにアクセスして、アメリカドルを基準に各種レートを得ます。
+            request("https://api.fixer.io/latest?base=USD", (error, response, body) => {
+                // 通貨レートAPIの結果をログ出力します。
+                console.log(`Fixer API response: ${JSON.stringify(response)} body: ${body} | error: ${error}`);
+                // 通貨レートAPIの結果をJSONにパースして、各種値を得ます。
+                const rates = JSON.parse(body).rates;
+                // DialogflowのWebhookで渡された通貨パラメータを取得します。
+                let currency = app.getArgument("currency");
+                // 通貨パラメータごとに処理分岐します。
+                if (currency === "円") {
+                    // "円" だった場合は、日本円のレートで計算します。
+                    price *= rates.JPY;
+                } else if (currency === "ユーロ") {
+                    // "ユーロ" だった場合は、ユーロのレートで計算します。
+                    price *= rates.EUR;
+                } else {
+                    // "円" でも "ユーロ" でもなかった場合は、アメリカドルのままとします。
+                    currency = "アメリカドル";
+                }
+                // 返事のメッセージ文字列を組み立てます。
+                const msg = `現在のビットコインの価格は、${price}${currency}です`;
+                // tell 関数を使って返事を行い、会話を終了します。
+                app.tell(msg);
+            });
+        });
+    };
+
+    // Bitcoinの総数を返事する "total" アクションのハンドラ関数です。
+    const totalHandler = (app) => {
+        // Bitcoin APIにアクセスして、Bitcoinの総数を得ます。
+        // Response Bodyには総数のみが送られてきます。
+        request(EXT_BITCOIN_API_URL + EXT_TOTAL, (error, response, body) => {
+            // Bitcoin APIの結果をログ出力します。
+            console.log(`totalHandler response: ${JSON.stringify(response)} Body: ${body} | Error: ${error}`);
+            // 総数の桁が大きいので、億の単位にします。
+            const total = body / 100000000;
+            // 返事のメッセージ文字列を組み立てます。
+            const msg = `現在、世界中で${total}億のビットコインがあります`;
+            // tell 関数を使って返事を行い、会話を終了します。
+            app.tell(msg);
+        });
+    };
+
+    // アクションのハンドラ関数のマップを作ります。
+    const actionMap = new Map();
+    actionMap.set(ACTION_PRICE, priceHandler);
+    actionMap.set(ACTION_TOTAL, totalHandler);
+
+    // ハンドラ関数のマップを渡して、リクエストを処理します。
+    app.handleRequest(actionMap);
+};
+// [END bitcoinInfo2]
